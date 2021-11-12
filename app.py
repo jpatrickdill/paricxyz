22488f36c1d0aa0dd9ffb6ac49b80ad5d10754c2
Patrick Dill <jamespatrickdill@gmail.com>
2021-11-12 12:22:14 -0500

    enforce https

--- app.py
+++ app.py
@@ -1,29 +1,39 @@
 import json
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
-indev = os.environ.get("dev")
+indev = True if os.environ.get("dev") else False
 
+print(indev)
 server_name = None
 if not indev:
     server_name = "paric.xyz"
 
 app = Flask(__name__, static_url_path="/", static_folder="static")
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 with open("./static/projects.json") as file:
     projects = json.load(file)
 
 
-@app.route('/')
+@app.before_request
+def before_request():
+    if not indev:
+        if not request.is_secure:
+            url = request.url.replace('http://', 'https://', 1)
+            code = 301
+            return redirect(url, code=code)
+
+
+@app.route("/")
 def hello_world():
     return render_template("index.html", projects=projects["projects"])
 
 
-@app.route('/resume')
+@app.route("/resume")
 def portfolio():
     return render_template("portfolio.html", projects=projects["projects"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)  # server_name=server_name, debug=True)
